ff8d065381977e39f0753a90391db84f9e952577
acimo <acimovic90@gmail.com>
2017-10-16 17:20:41 +0200

    ikke færdig

--- app/src/main/java/com/example/acimo/devicedetector/SignupAcivity.java
+++ app/src/main/java/com/example/acimo/devicedetector/SignupAcivity.java
@@ -1,6 +1,7 @@
 package com.example.acimo.devicedetector;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.EditText;
 public class SignupAcivity extends AppCompatActivity {
 
     private EditText createEmailText;
+    private EditText createUserText;
+    private EditText createPasswordText;
     private Button createUserButton;
     public static final String EMAIL_KEY = "email_input";
     @Override
@@ -18,6 +21,8 @@ public class SignupAcivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup_acivity);
 
         createEmailText = (EditText) findViewById(R.id.text_create_email);
+        createUserText = (EditText) findViewById(R.id.text_create_username);
+        createPasswordText = (EditText) findViewById(R.id.text_create_password);
         createUserButton = (Button) findViewById(R.id.button_create);
         createUserButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -27,5 +32,19 @@ public class SignupAcivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        createUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AsyncTaskRunner runner = new AsyncTaskRunner();
+                String checkUsername = createUserText.getText().toString();
+                String checkPassword = createPasswordText.getText().toString();
+                runner.execute(sleepTime);
+            }
+        });
+    }
+
+    private class AsyncTaskRunner {
     }
 }
